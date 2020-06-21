1a8a9a0862663ef57aa0f0335210bd6fa9e89954
Mark Zuschlag <markazuschlag@gmail.com>
2020-06-21 13:50:38 -0700

    Add semantic analysis to resolve variable bindings.

--- src/parser/resolver.rs
+++ src/parser/resolver.rs
@@ -6,15 +6,15 @@ use crate::parser::statement::{Stmt, Declarations};
 use crate::error::report::error;
 use std::collections::HashMap;
 
-pub struct Resolver {
-    interpreter: Interpreter,
+pub struct Resolver<'a> {
+    interpreter: &'a mut Interpreter,
     scopes: Vec<HashMap<String, bool>>
 }
 
 type ResolverError = Result<(), String>;
 
-impl Resolver {
-    fn new(&self, interpreter: Interpreter) -> Resolver {
+impl<'a> Resolver<'a> {
+    pub fn new(interpreter: &mut Interpreter) -> Resolver {
         let scopes = Vec::new();
         Resolver {
             interpreter,
@@ -22,20 +22,30 @@ impl Resolver {
         }
     }
 
+    pub fn resolve(&mut self, statements: &Declarations) -> ResolverError {
+        for statement in statements {
+            self.visit_stmt(statement)?;
+        }
+        Ok(())
+    }
+
+    fn visit_stmt(&mut self, statement: &Stmt) -> ResolverError {
+        match statement {
+            Stmt::Block(body) => self.visit_block_stmt(body),
+            Stmt::Var(name, initializer) => self.visit_var_stmt(name, initializer),
+            Stmt::Expression(expr) => self.visit_expression_stmt(expr),
+            Stmt::If(condition, then_branch, else_branch) => self.visit_if_stmt(condition, then_branch, else_branch),
+            Stmt::Print(expr) => self.visit_print_stmt(expr),
+            Stmt::Return(keyword, value) => self.visit_return_stmt(keyword, value),
+            Stmt::While(condition, body) => self.visit_while_stmt(condition, body),
+            Stmt::Function(name, params, body) => self.visit_function_stmt(name, params, body)
+        }
+    }
+
     fn visit_block_stmt(&mut self, statements: &Declarations) -> ResolverError {
         self.begin_scope();
         self.resolve(statements)?;
         self.end_scope();
-        Ok(())
-    }
-
-    fn resolve(&mut self, statements: &Declarations) -> ResolverError {
-        for statement in statements {
-            match statement {
-                Stmt::Var(name, initializer) => self.visit_var_stmt(name, initializer)?,
-                _ => ()
-            };
-        }
         Ok(())
     }
     
@@ -49,10 +59,54 @@ impl Resolver {
         Ok(())
     }
 
+    fn visit_expression_stmt(&mut self, expr: &Expr) -> ResolverError {
+        self.visit_expr(expr)
+    }
+
+    fn visit_if_stmt(&mut self, condition: &Expr, then_branch: &Stmt, else_branch: &Option<Stmt>) -> ResolverError {
+        self.visit_expr(condition)?;
+        self.visit_stmt(then_branch)?;
+        if let Some(statement) = else_branch {
+            self.visit_stmt(statement)?;
+        }
+        Ok(())
+    }
+
+    fn visit_print_stmt(&mut self, expr: &Expr) -> ResolverError {
+        self.visit_expr(expr)
+    }
+
+    #[allow(unused_variables)]
+    fn visit_return_stmt(&mut self, keyword: &Token, value: &Expr) -> ResolverError {
+        match value {
+            Expr::Literal(Literal::Nothing) => Ok(()),
+            _ => self.visit_expr(value)
+        }
+    }
+
+    fn visit_while_stmt(&mut self, condition: &Expr, body: &Stmt) -> ResolverError {
+        self.visit_expr(condition)?;
+        self.visit_stmt(body)
+    }
+
+    fn visit_function_stmt(&mut self, name: &Token, params: &Vec<Token>, body: &Declarations) -> ResolverError {
+        self.declare(name);
+        self.define(name);
+        self.resolve_function(params, body)
+    }
+
     fn visit_expr(&mut self, expr: &Expr) -> ResolverError {
         match expr {
-            Expr::Variable(name) => self.visit_variable_expr(name),
-            _ => Ok(())
+            Expr::Variable(var) => self.visit_variable_expr(var),
+            Expr::Assign(name, value) => self.visit_assign_expr(name, value),
+            Expr::Binary(left, _, right) => self.visit_binary_expr(left, right),
+            Expr::Logical(left, _, right) => self.visit_logical_expr(left, right),
+            Expr::Ternary(left, middle, right) => self.visit_ternary_expr(left, middle, right),
+            Expr::Grouping(group) => self.visit_grouping_expr(group),
+            Expr::Unary(_, right) => self.visit_unary_expr(right),
+            Expr::Call(callee, _, arguments) => self.visit_call_expr(callee, arguments),
+            Expr::Lambda(args, body) => self.visit_lambda_expr(args, body),
+            Expr::Literal(_) => self.visit_literal()
         }
     }
 
@@ -63,6 +117,59 @@ impl Resolver {
             }
         }
         self.resolve_local(name);
+        Ok(())
+    }
+
+    fn visit_assign_expr(&mut self, name: &Token, value: &Expr) -> ResolverError {
+        self.visit_expr(value)?;
+        self.resolve_local(name);
+        Ok(())
+    }
+
+    fn visit_binary_expr(&mut self, left: &Expr, right: &Expr) -> ResolverError {
+        self.visit_expr(left)?;
+        self.visit_expr(right)
+    }
+
+    fn visit_logical_expr(&mut self, left: &Expr, right: &Expr) -> ResolverError {
+        self.visit_expr(left)?;
+        self.visit_expr(right)
+    }
+
+    fn visit_ternary_expr(&mut self, left: &Expr, middle: &Expr, right: &Expr) -> ResolverError {
+        self.visit_expr(left)?;
+        self.visit_expr(middle)?;
+        self.visit_expr(right)
+    }
+
+    fn visit_grouping_expr(&mut self, group: &Expr) -> ResolverError {
+        self.visit_expr(&group)
+    }
+
+    fn visit_unary_expr(&mut self, expr: &Expr) -> ResolverError {
+        self.visit_expr(expr)
+    }
+
+    fn visit_call_expr(&mut self, callee: &Expr, arguments: &Vec<Box<Expr>>) -> ResolverError {
+        self.visit_expr(callee)?;
+        for argument in arguments {
+            self.visit_expr(argument)?;
+        }
+        Ok(())
+    }
+
+    fn visit_lambda_expr(&mut self, params: &Vec<Token>, body: &Declarations) -> ResolverError {
+        self.begin_scope();
+        for param in params {
+            self.declare(param);
+            self.define(param);
+        }
+        self.resolve(body)?;
+        self.end_scope();
+        Ok(())
+    }
+
+    fn visit_literal(&self) -> ResolverError {
         Ok(())
     }
 
@@ -87,10 +194,27 @@ impl Resolver {
     }
 
     fn resolve_local(&mut self, name: &Token) {
-        for scope in self.scopes.iter().rev() {
-            if scope.contains_key(&name.lexeme) {
-                // Do something
+        if self.scopes.len() > 0 {
+            for i in (0..=self.scopes_depth()).rev() {
+                if self.scopes[i].contains_key(&name.lexeme) {
+                    self.interpreter.resolve(name, self.scopes_depth() - i)
+                }
             }
+        }   
+    }
+
+    fn resolve_function(&mut self, params: &Vec<Token>, body: &Declarations) -> ResolverError {
+        self.begin_scope();
+        for param in params {
+            self.declare(param);
+            self.define(param);
         }
+        self.resolve(body)?;
+        self.end_scope();
+        Ok(())
+    }
+
+    fn scopes_depth(&self) -> usize {
+        self.scopes.len() - 1
     }
 }
